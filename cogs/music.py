3a80848d12ad5edf91e95ac2ab3a249aa223ba5d
Angeloffy <angeloffy.work@gmail.com>
2023-08-07 17:49:35 +0300

    fix: Checking for bot access rights to connect & talk

--- cogs/music.py
+++ cogs/music.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake import FFmpegPCMAudio
 from disnake.ext import commands
-
+from bot import bot
 
 class radio_voice(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -13,9 +13,20 @@ class radio_voice(commands.Cog):
         await inter.response.defer()
         url_music = "http://stream.radioparadise.com"
         source = FFmpegPCMAudio(url_music)
-        a = await voice_channel.connect(timeout=10, reconnect=True)
-        a.play(source)
-        await inter.edit_original_message(content="Успешно!")
+        permision = voice_channel.permissions_for(inter.guild.me)
+        if permision.connect:
+            if permision.speak:
+                if inter.guild.voice_client is None:
+                    a = await voice_channel.connect(timeout=10, reconnect=True)
+                else:
+                    await inter.guild.voice_client.disconnect(force=True)
+                    a = await voice_channel.connect(timeout=10, reconnect=True)
+                a.play(source)
+                await inter.edit_original_message(content="Успешно!")
+            else:
+                await inter.edit_original_message(content="Не достаточно прав на говорить!")
+        else:
+            await inter.edit_original_message(content="Не достаточно прав на подключение!")
 
 def setup(bot):
     bot.add_cog(radio_voice(bot))
